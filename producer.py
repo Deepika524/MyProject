88db59bf0739f95b9d66c0ca5c12a2567f044677
Deepika524 <87222749+Deepika524@users.noreply.github.com>
2024-01-30 18:51:59 +0530

    producer.py

--- producer.py
+++ producer.py
@@ -12,15 +12,15 @@ from dotenv import load_dotenv
 
 load_dotenv('.env')
 
-bootstrap_servers=['localhost:9092']
+TOPIC_NAME="StockData"
+KAFKA_SERVER=['localhost:9092']
+producer=KafkaProducer(bootstrap_servers=KAFKA_SERVER,value_serializer=lambda K:dumps(K).encode('utf-8'))
 
-producer = KafkaProducer(bootstrap_servers=bootstrap_servers,value_serializer=lambda K:dumps(K).encode('utf-8'))
 
-demo = os.getenv('API_KEY')
+key = os.getenv('API_KEY')
 
-#replace the "demo" apikey below with your own key from https://www.alphavantage.co/suppines SV_URL = 'https://www.alphavantage.co/query?function=TIME SERIES INTRADAY EXTENDED&G)
 
-CSV_URL="https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDES&symbol=IBM&interval=5min&slice=year1month1&apikey={}".format(demo)
+CSV_URL="https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=1min&slice=year1month1&apikey={}".format(key)
 with requests. Session() as s:
 
     download = s.get(CSV_URL)
@@ -33,4 +33,4 @@ with requests. Session() as s:
 
     for row in my_list:
 
-        producer.send('text',row)
+        producer.send(TOPIC_NAME,row)
