7b63b7d9de369469f2f1a0671fbc76466318bfb0
Deepika524 <87222749+Deepika524@users.noreply.github.com>
2024-02-08 20:38:08 +0530

    producer.py

--- producer.py
+++ producer.py
@@ -1,36 +1,26 @@
 import csv
-
 import requests
-
-import os
-
 from kafka import KafkaProducer
-
 from json import dumps
 
-from dotenv import load_dotenv
+TOPIC_NAME = "StockData"
+KAFKA_SERVER = ['localhost:9092']
+producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER, value_serializer=lambda K: dumps(K).encode('utf-8'))
 
-load_dotenv('.env')
-
-TOPIC_NAME="StockData"
-KAFKA_SERVER=['localhost:9092']
-producer=KafkaProducer(bootstrap_servers=KAFKA_SERVER,value_serializer=lambda K:dumps(K).encode('utf-8'))
-
-
-key = os.getenv('API_KEY')
-
-
-CSV_URL="https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=1min&apikey=2OWSX3KJQKVEWV6D&datatype=csv"
-with requests. Session() as s:
-
+API_KEY = "2OWSX3KJQKVEWV6D"
+CSV_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=1min&apikey=2OWSX3KJQKVEWV6D&datatype=csv"
+with requests.Session() as s:
     download = s.get(CSV_URL)
-    
     decoded_content = download.content.decode('utf-8')
-
     cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    header = next(cr)  # Get the header
+    my_list_of_dicts = []
 
-    my_list = list(cr)
+    for row in cr:
+        data_dict = dict(zip(header, row))
+        my_list_of_dicts.append(data_dict)
+        producer.send(TOPIC_NAME, data_dict)
+        print(data_dict)  # For testing, print each dictionary
 
-    for row in my_list:
+producer.close()  # Close the producer after sending data
 
-        producer.send(TOPIC_NAME,row)
